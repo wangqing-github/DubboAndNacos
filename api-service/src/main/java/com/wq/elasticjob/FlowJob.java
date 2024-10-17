fc4f301a8f9f08a71910cc16776e63d08c0dbcfa
wangqing <751244353@qq.com>
2024-10-17 14:37:52 +0800

    elastic-job简单示例

--- api-service/src/main/java/com/wq/elasticjob/FlowJob.java
+++ api-service/src/main/java/com/wq/elasticjob/FlowJob.java
@@ -1,19 +1,25 @@
-package elasticjob;
+package com.wq.elasticjob;
 
 import beans.UserInfo;
 import org.apache.shardingsphere.elasticjob.api.ShardingContext;
 import org.apache.shardingsphere.elasticjob.dataflow.job.DataflowJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class FlowJob implements DataflowJob<UserInfo> {
+    private static final Logger logger = LoggerFactory.getLogger(FlowJob.class);
+
     @Override
     public List<UserInfo> fetchData(ShardingContext shardingContext) {
         List<UserInfo> foos = new ArrayList<>();
         double random = Math.random();
-        System.out.println("fetchData------ " + random);
-        if (random > 0.5){
+        if (random > 0.5) {
+            logger.info("fetchData------ {}", random);
             UserInfo foo = new UserInfo();
             foo.setUserName("小道仙");
             foos.add(foo);
@@ -23,6 +29,7 @@ public class FlowJob implements DataflowJob<UserInfo> {
 
     @Override
     public void processData(ShardingContext shardingContext, List<UserInfo> list) {
-
+        logger.info("收到流数据");
+        list.forEach(i -> logger.info("name为：{}", i.getUserName()));
     }
 }
